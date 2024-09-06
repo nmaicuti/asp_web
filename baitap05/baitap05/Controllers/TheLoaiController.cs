b1f10aca0d7cb23b3f6d08a517b9a63ea64e4c8a
nmaicuti <nguyenngocmai082004@gmail.com>
2024-09-06 14:41:31 +0700

    update

---
+++ baitap05/baitap05/Controllers/TheLoaiController.cs
@@ -0,0 +1,26 @@
+﻿using baitap05.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace baitap05.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Ngay = "Ngày 28";
+            ViewBag.Thang = "Tháng 02";
+            ViewData["Nam"] = "Năm 2030";
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            var theloai = new TheLoaiViewModels
+            {
+                Id = 1,
+                Name = "Trinh thám"
+
+            };
+            return View(theloai);
+        }
+    }
+}
---
+++ baitap05/baitap05/Models/TheLoaiViewModels.cs
@@ -0,0 +1,9 @@
+﻿namespace baitap05.Models
+{
+    public class TheLoaiViewModels
+
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
