1be92d0adb975c8907b7d26172c7f3dcc33c4df1
nmaicuti <nguyenngocmai082004@gmail.com>
2024-09-07 15:15:07 +0700

    baitapvenha02

---
+++ baitapthuchanh/baitapthuchanh/Controllers/Tuan02Controller.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace baitapthuchanh.Controllers
+{
+    public class Tuan02Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HoTen = "Nguyen Ngoc Mai";
+            ViewBag.MSSV = "1822040747";
+            ViewBag.Nam = 2004;
+            return View();
+        }
+        public IActionResult MayTinh(int a, int b, string pheptinh)
+        {
+            double PhepTinh;
+            if (pheptinh == "cong")
+                PhepTinh = a + b;
+            else if (pheptinh == "tru")
+                PhepTinh = a - b;
+            else if (pheptinh == "nhan")
+                PhepTinh = a * b;
+            else
+                PhepTinh = (double)a / b;
+            ViewBag.KetQua = PhepTinh;
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
