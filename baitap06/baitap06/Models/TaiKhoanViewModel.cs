cc6630389a17251aaadedbc3132480983327cf2c
nmaicuti <nguyenngocmai082004@gmail.com>
2024-09-09 15:26:00 +0700

    baitap06

---
+++ baitap06/baitap06/Models/TaiKhoanViewModel.cs
@@ -0,0 +1,12 @@
+﻿
+namespace baitap06.Models
+{
+    public class TaiKhoanViewModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
