850b0f6a022800a5531f45e459abb8ea4e840089
nmaicuti <nguyenngocmai082004@gmail.com>
2024-09-16 17:15:39 +0700

    update

---
+++ baitap07a/baitap07a/Controllers/TheLoaiController.cs
@@ -0,0 +1,27 @@
+﻿using baitap07a.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace baitap07a.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TheLoaiController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var theloai = _db.TheLoai.ToList();
+            ViewBag.TheLoai = theloai;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ baitap07a/baitap07a/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using baitap07a.Models;
+using baitap07a.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace baitap07a.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<TheLoai> TheLoai { get; set; }
+    }
+}
