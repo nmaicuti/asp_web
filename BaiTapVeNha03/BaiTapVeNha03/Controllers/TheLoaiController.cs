fbbb5b6d55949fbe8cd10573914bd87816873ee7
nmaicuti <nguyenngocmai082004@gmail.com>
2024-09-23 13:07:07 +0700

    baitapvenha03

---
+++ BaiTapVeNha03/BaiTapVeNha03/Controllers/TheLoaiController.cs
@@ -0,0 +1,108 @@
+﻿using BaiTapVeNha03.Data;
+using BaiTapVeNha03.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapVeNha03.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public TheLoaiController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+           var theloai = _db.TheLoai
+                            .Where(t => t.Id < 3)
+                            .ToList();
+            ViewBag.TheLoai = theloai;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(TheLoai theLoai)
+        {
+            if (ModelState.IsValid)
+            {
+                //them thong tin
+                _db.TheLoai.Add(theLoai);
+                //luu lai thong tin
+                _db.SaveChanges();
+                //chuyen ve trang index
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var theloai = _db.TheLoai.Find(id);
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(TheLoai theLoai)
+        {
+            if (ModelState.IsValid)
+            {
+                //them thong tin
+                _db.TheLoai.Update(theLoai);
+                //luu lai thong tin
+                _db.SaveChanges();
+                //chuyen ve trang index
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var theloai = _db.TheLoai.Find(id);
+            return View(theloai);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirm(int id)
+        {
+            var theloai = _db.TheLoai.Find(id);
+            if (theloai == null)
+            {
+                return NotFound();
+            }
+            _db.TheLoai.Remove(theloai);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var theloai = _db.TheLoai.Find(id);
+            return View(theloai);
+        }
+    }
+}
---
+++ BaiTapVeNha03/BaiTapVeNha03/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BaiTapVeNha03.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaiTapVeNha03.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<TheLoai> TheLoai{ get; set; }
+    }
+}
