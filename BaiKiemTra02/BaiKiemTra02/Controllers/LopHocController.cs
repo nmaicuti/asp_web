7ff456ff07bb583a707907e9edb44b66182f563c
nmaicuti <nguyenngocmai082004@gmail.com>
2024-09-24 15:04:57 +0700

    baikiemtra2

---
+++ BaiKiemTra02/BaiKiemTra02/Controllers/LopHocController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BaiKiemTra02.Data;
+using BaiKiemTra02.Models;
+
+namespace BaiKiemTra02.Controllers
+{
+    public class LopHocController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LopHocController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: LopHoc
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.LopHocs.ToListAsync());
+        }
+
+        // GET: LopHoc/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lopHoc = await _context.LopHocs
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (lopHoc == null)
+            {
+                return NotFound();
+            }
+
+            return View(lopHoc);
+        }
+
+        // GET: LopHoc/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LopHoc/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,TenLopHoc,NamNhapHoc,NamRaTruong,SoLuongSinhVien")] LopHoc lopHoc)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(lopHoc);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(lopHoc);
+        }
+
+        // GET: LopHoc/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lopHoc = await _context.LopHocs.FindAsync(id);
+            if (lopHoc == null)
+            {
+                return NotFound();
+            }
+            return View(lopHoc);
+        }
+
+        // POST: LopHoc/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,TenLopHoc,NamNhapHoc,NamRaTruong,SoLuongSinhVien")] LopHoc lopHoc)
+        {
+            if (id != lopHoc.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(lopHoc);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LopHocExists(lopHoc.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(lopHoc);
+        }
+
+        // GET: LopHoc/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lopHoc = await _context.LopHocs
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (lopHoc == null)
+            {
+                return NotFound();
+            }
+
+            return View(lopHoc);
+        }
+
+        // POST: LopHoc/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var lopHoc = await _context.LopHocs.FindAsync(id);
+            _context.LopHocs.Remove(lopHoc);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LopHocExists(int id)
+        {
+            return _context.LopHocs.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ BaiKiemTra02/BaiKiemTra02/Models/LopHoc.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaiKiemTra02.Models
+{
+    public class LopHoc
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string TenLopHoc { get; set; }
+
+        [Required]
+        [Range(1900, 2100)]
+        public int NamNhapHoc { get; set; }
+
+        [Required]
+        [Range(1900, 2100)]
+        public int NamRaTruong { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int SoLuongSinhVien { get; set; }
+    }
+}
